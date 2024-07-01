3efd927ea196c71754b93e43bd2bd729ea4500d3
Alexandre <phasmacaptain>
2024-07-01 17:53:59 -0400

    auth lambda

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -14,21 +14,6 @@ resource "aws_lambda_function" "auth_sign_up" {
   }
 }
 
-resource "aws_lambda_function" "auth_sign_in" {
-  function_name = "auth-sign-in"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  role          = var.lambda_exec_role
-  filename      = "path/to/auth-sign-in.zip"
-}
-
-resource "aws_lambda_function" "auth_authorizer" {
-  function_name = "auth-authorizer"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  role          = var.lambda_exec_role
-  filename      = "path/to/auth-authorizer.zip"
-}
 
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
