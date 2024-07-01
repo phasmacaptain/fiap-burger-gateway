7ddc51c1c8aa97c32f3d85d2b6d2214c6f59f564
Alexandre <phasmacaptain>
2024-07-01 18:52:07 -0400

    auth lambda

--- modules/api_gateway/balancers.tf
+++ modules/api_gateway/balancers.tf
@@ -1,9 +1,29 @@
+resource "aws_security_group" "lb_sg" {
+  name        = "fiap-burger-lb-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lb" "clients_fiap_burger_lb" {
   name               = "fiap-burger-lb-clients"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
+  subnets            = ["subnet-07559b6ca5d4e7ab4", "subnet-060aa625708d1da40", "subnet-003119a8b68a3fe05"]
 }
 
 resource "aws_lb_target_group" "clients_fiap_burger_tg" {
@@ -29,7 +49,7 @@ resource "aws_lb" "pedidos_fiap_burger_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
+  subnets            = ["subnet-07559b6ca5d4e7ab4", "subnet-060aa625708d1da40", "subnet-003119a8b68a3fe05"]
 }
 
 resource "aws_lb_target_group" "pedidos_fiap_burger_tg" {
@@ -55,7 +75,7 @@ resource "aws_lb" "produtos_fiap_burger_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
+  subnets            = ["subnet-07559b6ca5d4e7ab4", "subnet-060aa625708d1da40", "subnet-003119a8b68a3fe05"]
 }
 
 resource "aws_lb_target_group" "produtos_fiap_burger_tg" {
@@ -81,7 +101,7 @@ resource "aws_lb" "checkout_fiap_burger_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
+  subnets            = ["subnet-07559b6ca5d4e7ab4", "subnet-060aa625708d1da40", "subnet-003119a8b68a3fe05"]
 }
 
 resource "aws_lb_target_group" "checkout_fiap_burger_tg" {
@@ -107,7 +127,7 @@ resource "aws_lb" "categorias_fiap_burger_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
+  subnets            = ["subnet-07559b6ca5d4e7ab4", "subnet-060aa625708d1da40", "subnet-003119a8b68a3fe05"]
 }
 
 resource "aws_lb_target_group" "categorias_fiap_burger_tg" {
