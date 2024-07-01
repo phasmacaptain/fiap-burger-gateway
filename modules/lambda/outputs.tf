c29608ed69cfa3bd7e36282781822927b14191f1
Alexandre <phasmacaptain>
2024-07-01 17:57:16 -0400

    auth lambda

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -5,7 +5,3 @@ output "auth_sign_up_arn" {
 output "auth_sign_in_arn" {
   value = aws_lambda_function.auth_sign_in.arn
 }
-
-output "auth_authorizer_arn" {
-  value = aws_lambda_function.auth_authorizer.arn
-}
