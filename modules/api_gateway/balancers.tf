dca89e1e3106e2395ab3a737cbc51b511707ab97
Alexandre <phasmacaptain>
2024-07-15 22:38:15 -0300

    ˜teste˜

--- modules/api_gateway/balancers.tf
+++ modules/api_gateway/balancers.tf
@@ -115,7 +115,6 @@ resource "aws_lb_listener" "checkout_fiap_burger_listener" {
   load_balancer_arn = aws_lb.checkout_fiap_burger_lb.arn
   port              = "80"
   protocol          = "HTTP"
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.checkout_fiap_burger_tg.arn
